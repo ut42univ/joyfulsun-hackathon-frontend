55d4f782bffe1782884e8dd885c24f7fb20dbd48
tmba <ut42.nu@gmail.com>
2025-01-28 15:09:47 +0900

    Headerコンポーネントの高さを24から20に変更

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <header className="fixed w-full h-24 z-50 flex justify-between items-center px-16 bg-neutral-800 text-white">
+    <header className="fixed w-full h-20 z-50 flex justify-between items-center px-16 bg-neutral-800 text-white">
       <Link href="/" className="font-medium text-sm flex gap-4 items-center">
         <div className="w-12 h-12 rounded-full bg-orange-500" />
         <p>Joyful Sun Data Integration</p>
