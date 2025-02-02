66c7b99264691bbcaae609cd5c2965a61e0b7985
tmba <ut42.nu@gmail.com>
2025-02-02 23:37:16 +0900

    ABCおよびRFM分析ページにデータ取得のためのuseEffectフックを追加し、ChartABCコンポーネントを表示

--- src/app/dashboard/analysis/abc/page.tsx
+++ src/app/dashboard/analysis/abc/page.tsx
@@ -1,13 +1,29 @@
 "use client";
 
+import { ChartABC } from "@/components/elements/ChartACB";
 import { CustomBreadcrumb } from "@/components/layouts/CustomBreadcrumb";
 import { useVersion } from "@/contexts/VersionContext";
+import { Month, StoreName, tableName } from "@/lib/dictTable";
+import { ResABC } from "@/types";
+import { getABCData } from "@/utils/apiUtils";
+import { useEffect } from "react";
 
 const ABCAnalysisPage = () => {
   const { selectedVersion } = useVersion();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const table: string = tableName(selectedVersion as StoreName, 7 as Month);
+      const resABC: ResABC[] = await getABCData(table);
+      console.log(resABC);
+    };
+    fetchData();
+  }, [selectedVersion]);
+
   return (
     <div>
       <CustomBreadcrumb selectedVersion={selectedVersion} pageName="ABC分析" />
+      <ChartABC />
     </div>
   );
 };
--- src/app/dashboard/analysis/rfm/page.tsx
+++ src/app/dashboard/analysis/rfm/page.tsx
@@ -2,9 +2,22 @@
 
 import { CustomBreadcrumb } from "@/components/layouts/CustomBreadcrumb";
 import { useVersion } from "@/contexts/VersionContext";
+import { Month, StoreName, tableName } from "@/lib/dictTable";
+import { ResRFM } from "@/types";
+import { getRFMData } from "@/utils/apiUtils";
+import { useEffect } from "react";
 
 const RFMAnalysisPage = () => {
   const { selectedVersion } = useVersion();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const table: string = tableName(selectedVersion as StoreName, 7 as Month);
+      const resRFM: ResRFM[] = await getRFMData(table);
+      console.log(resRFM);
+    };
+    fetchData();
+  }, [selectedVersion]);
   return (
     <div>
       <CustomBreadcrumb selectedVersion={selectedVersion} pageName="RFM分析" />
